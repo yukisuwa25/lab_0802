75f0dec97c7d8e9f707d5bd4f6e8747816813144
yukisuwa25 <yuki.997.777@gmail.com>
2023-08-02 16:07:24 +0900

    ファイル

---
+++ src/pages/news.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from '../styles/test.module.scss'
+
+const news = () => {
+  return (
+    <div className='text-5xl'>
+      news
+      <p className={style.test}>
+        テスト
+      </p>
+    </div>
+  )
+}
+
+export default news
